3b73c9b605555c0dc19e9e7b42cd5dfbc802e03c
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2021-03-22 09:41:40 -0500

    test command

--- commands/test.js
+++ commands/test.js
@@ -1,6 +1,10 @@
 const fetch = require('node-fetch');
+const { version } = require('../package.json')
 module.exports = {
-    run() {
-        fetch(``)
+    async run() {
+        const data = await fetch(`https://raw.githubusercontent.com/L061571C5/Music-Downloader/main/package.json`)
+        const json = await data.json()
+        if (json.version !== version) return console.log(`This version is out of date, please update.`)
+        console.log(`This version ${version}, is the latest.`)
     }
 }
