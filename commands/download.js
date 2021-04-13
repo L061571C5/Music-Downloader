fb98d4c12debc7d338daa4a2c6278b9eabc394e8
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2021-04-13 17:07:47 -0500

    whoops

--- commands/download.js
+++ commands/download.js
@@ -19,12 +19,12 @@ const r = new snoowrap({
 });
 module.exports = {
     async run(args) {
-        let type = -1;
+        let type = 0;
         if (this.validYTPlaylistURL(args[0])) type = 1
         else if (this.validYTURL(args[0])) type = 2
         else if (this.validKHURL(args[0])) type = 3
         else if (args[0] == 'reddit') type = 4
-        if (this.validYTURL(args[0]) == false && type === -1) {
+        if (this.validYTURL(args[0]) == false && args.length <= 2) {
             console.log(chalk.greenBright(`Downloader Usage:`))
             return console.log(chalk.whiteBright(`node index.js dl (url or query) [options]`))
         }
